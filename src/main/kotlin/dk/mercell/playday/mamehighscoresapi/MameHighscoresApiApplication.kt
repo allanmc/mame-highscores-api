1ba6509b23fef215819cd81dc96945241d7c2b82
allanmc <allanmc@gmail.com>
2020-08-24 10:32:16 +0200

    Initial commit of Spring Initializr

---
+++ src/main/kotlin/dk/mercell/playday/mamehighscoresapi/MameHighscoresApiApplication.kt
@@ -0,0 +1,11 @@
+package dk.mercell.playday.mamehighscoresapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MameHighscoresApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<MameHighscoresApiApplication>(*args)
+}
---
+++ src/test/kotlin/dk/mercell/playday/mamehighscoresapi/MameHighscoresApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package dk.mercell.playday.mamehighscoresapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MameHighscoresApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

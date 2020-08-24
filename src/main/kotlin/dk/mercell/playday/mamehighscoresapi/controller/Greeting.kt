da70c2f212885675c692505bcef608c8a0086713
Timo Leinonen <splinecl@gmail.com>
2020-08-24 13:32:05 +0200

    Stuff

--- src/main/kotlin/dk/mercell/playday/mamehighscoresapi/controller/Greeting.kt
+++ src/main/kotlin/dk/mercell/playday/mamehighscoresapi/controller/Greeting.kt
@@ -1,3 +1,3 @@
 package dk.mercell.playday.mamehighscoresapi.controller
 
-data class Greeting(val id: Long, val content: String)
+data class Greeting(val game: String, val player: String, val score: Long)
--- src/main/kotlin/dk/mercell/playday/mamehighscoresapi/controller/MainController.kt
+++ src/main/kotlin/dk/mercell/playday/mamehighscoresapi/controller/MainController.kt
@@ -1,5 +1,6 @@
 package dk.mercell.playday.mamehighscoresapi.controller
 
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -9,7 +10,8 @@ import java.util.concurrent.atomic.AtomicLong
 class MainController {
     val counter = AtomicLong()
 
+    @CrossOrigin(origins = ["http://localhost:3000"])
     @GetMapping("/greeting")
     fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String) =
-            Greeting(counter.incrementAndGet(), "Hello, $name")
+            arrayListOf(Greeting("mslug", "TCL", 100000),Greeting("ddp2", "TCL", 325325))
 }
